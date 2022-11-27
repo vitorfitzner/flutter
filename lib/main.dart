a92abe2158af47b617f1804b8b0b0958e8fa07e3
Vitor Fitzner <vitorfitzner@gmail.com>
2022-11-27 17:53:47 -0300

    using material components

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Center(
-      child: Text('Hello Wasdforld', textDirection: TextDirection.ltr)));
+  runApp(const MaterialApp(
+    title: 'Flutter with Material Design', 
+    home: SafeArea(
+      child: MyScaffold()
+    ))
+  );
+}
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({required this.title, super.key});
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          const IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Navigation Menu',
+              onPressed: null),
+          Expanded(child: title),
+          const IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search),
+            tooltip: 'Search button',
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: [
+          MyAppBar(title: Text('Titulo de exemplo',style: Theme.of(context).primaryTextTheme.headline6)),
+          const Expanded(child: Center(child: Text('Hello, World!')))
+        ],
+    ));
+  }
 }
