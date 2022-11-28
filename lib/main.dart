6fc78699db7e55da3925ec8ec683dd7f72fe02bd
Vitor Fitzner <vitorfitzner@gmail.com>
2022-11-27 22:02:28 -0300

    Handling gestures

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    title: 'Flutter with Material Design', 
-    home: SafeArea(
-      child: MyScaffold()
-    ))
-  );
+    title: 'Flutter with Material Design',
+    home: Scaffold(
+      body: Center(
+        child: MyButton()
+      )
+    )
+  ));
 }
 
 class MyAppBar extends StatelessWidget {
@@ -45,10 +47,30 @@ class MyScaffold extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: Column(
-        children: [
-          MyAppBar(title: Text('Titulo de exemplo',style: Theme.of(context).primaryTextTheme.headline6)),
-          const Expanded(child: Center(child: Text('Hello, World!')))
-        ],
+      children: [
+        MyAppBar(
+            title: Text('Titulo de exemplo',
+                style: Theme.of(context).primaryTextTheme.headline6)),
+        const Expanded(child: Center(child: Text('Hello, World!')))
+      ],
     ));
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () { print('Botão acionado'); },
+      child: Container(
+        height: 50.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(5.0)),
+        child: const Center(child: Text('Engage'))
+      )
+    );
   }
 }
